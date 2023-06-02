9a415212ee8f13e250a07c7cec751b3dfa7f6f4f
camilogs <camilo.gonzalez.col@gmail.com>
2023-06-01 20:13:54 -0500

    Add sale details modele

---
+++ WebStore/Models/SaleDetailsModel.cs
@@ -0,0 +1,12 @@
+namespace WebStore.Models;
+
+public class SaleDetailsModel
+{
+    public int SaleDetailsId { get; set;}        
+
+    public int Amount { get; set;}
+    public double Total { get; set;}
+
+    public int SaleId { get; set;}
+    public int ProductId { get; set;}
+}
